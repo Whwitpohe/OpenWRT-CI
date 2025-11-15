5f36e56472e837f8719e17065cc2abd812375519
WHWITPOHE\fushi <fushitx@hotmail.com>
2025-11-15 22:10:15 +0800

    fix

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -4,7 +4,11 @@
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改immortalwrt.lan关联IP
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+
 #添加编译日期标识
+cd ./feeds/luci
+git restore .
+cd $GITHUB_WORKSPACE
 sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 
 WIFI_SH=$(find ./target/linux/{mediatek/filogic,qualcommax}/base-files/etc/uci-defaults/ -type f -name "*set-wireless.sh" 2>/dev/null)
